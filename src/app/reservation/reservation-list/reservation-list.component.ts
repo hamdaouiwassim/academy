d1ada69489226930a9c474afae311fcbf30ab5b6
hamdaouiwassim <hamdaouiwassim@gmail.com>
2020-07-12 23:35:18 +0200

    reservations

--- src/app/reservation/reservation-list/reservation-list.component.ts
+++ src/app/reservation/reservation-list/reservation-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/event.service';
+import { BookService } from 'src/app/book.service';
+import { UserService } from 'src/app/shared/user.service';
+import { ReserveEventService } from 'src/app/shared/reserveEvent/reserve-event.service';
+import { ReservebookService } from 'src/app/shared/reservebook/reservebook.service';
 
 @Component({
   selector: 'app-reservation-list',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventsService : EventService , private booksService : BookService , private usersService : UserService  , private reservebookService : ReservebookService , private  reserveeventService : ReserveEventService) { }
+  reservebooks : any;
+  reserveevents : any;
+  users : any;
+  events : any;
+  books : any;
 
   ngOnInit(): void {
+    this.getreservebook();
+    this.getreserveevent();
+    this.getBooks();
+    this.getEvents();
+    this.getUsers();
   }
+
+  getreservebook(){
+    this.reservebookService.getAllReservebooks().subscribe(res=>{
+       this.reservebooks = res; 
+    },err=>{
+
+    })
+  }
+  
+  getreserveevent(){
+    this.reserveeventService.getAllReserveevents().subscribe(res=>{
+       this.reserveevents = res; 
+    },err=>{
+
+    })
+  }
+  getUsers(){
+    this.usersService.getallusers().subscribe(res=>{
+      this.users = res;
+      console.log(res);
+    },err=>{
+      
+    })
+    
+  }
+
+  getBooks(){
+    this.booksService.get().subscribe(res=>{
+      this.books = res;
+      console.log(res);
+    },err=>{
+      
+    })
+    
+  }
+
+  getEvents(){
+    this.eventsService.get().subscribe(res=>{
+      this.events = res;
+      console.log(res);
+    },err=>{
+      
+    })
+    
+  }
+
 
 }
